c22e4c6386a69fb208670a1342452c6389064e1a
God-Spirit <amobiv3@gmail.com>
2022-03-14 05:49:38 -0700

    My size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	int long_int = 01;
 	int long_long_int = 011;
 	float float_num = 0.0f;
-	
-	printf(("Size of a char: %d byte(s)\n", sizeof(char));
+
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
-	printf("Size of a long long int: %lld byte(s)\n", sizeof(long long));
+	printf("Size of long long int: %lld byte(s)\n", sizeof(long long));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
