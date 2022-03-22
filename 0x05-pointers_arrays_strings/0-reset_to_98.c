b2b6849698fd886754a20a985d4fd8bf19ab82f1
God-Spirit <amobiv3@gmail.com>
2022-03-22 05:03:35 -0700

    Update the value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it point to 98
- * @n: int parameter
+ *@n: int parameter
  * Return: Always 0;
- * */
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
