c41516d2ff17e9ce612420b6e54304678d7e1620
God-Spirit <amobiv3@gmail.com>
2022-04-15 14:59:00 -0700

    Num of arg

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -27,3 +27,16 @@ int main(int argc, char *argv[])
 		exit(2);
 	}
 
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
